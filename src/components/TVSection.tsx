9f735bdd51021df7b89b4107d3248ed76472b6cd
ViniciusDev <vinicius.r.o.mendes@gmail.com>
2025-07-24 15:55:47 -0300

    ajuste

--- src/components/TVSection.tsx
+++ src/components/TVSection.tsx
@@ -8,21 +8,11 @@ export const TVSection = ({
   videoUrl = "/videos/video3.mp4" 
 }: TVSectionProps) => {
   return (
-    <div className="w-full h-full relative overflow-hidden">
-      <div 
-        className="absolute inset-0 bg-center bg-no-repeat bg-contain opacity-10 pointer-events-none"
-        style={{
-          backgroundImage: "url('/images/logo-background.svg')",
-          backgroundSize: 'contain',
-          backgroundPosition: 'center'
-        }}
+    <div className="w-full h-full bg-black">
+      <VideoPlayer 
+        videoUrl={videoUrl}
+        className="w-full h-full"
       />
-      <div className="relative z-10 w-full h-full">
-        <VideoPlayer 
-          videoUrl={videoUrl}
-          className="w-full h-full object-scale-down"
-        />
-      </div>
     </div>
   );
 };
